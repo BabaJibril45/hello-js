9f79ce400c265408dac5b6df0b950c34b7b48df8
Mohammed Baba Jibril <babajibril44@gmail.com>
2026-07-19 00:52:20 +0100

    feat: complete week2 Assignment

---
+++ math.js
@@ -0,0 +1,13 @@
+
+export function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Inputs must be numbers!'); // Error handling addition
+    }
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+module.export = { add, subtract }; // Export object
---
+++ server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log( `${req.method} ${req.url} - ${new Date()}`);
+  next(); 
+});
+
+
+app.get('/', (req, res) => {
+  res.send("My week 2 API!");
+});
+
+app.post('/user', (req, res) => {
+  const {name, email} = req.body;
+
+
+// Check for missing data (Return 400)
+if (!name || !email) {
+  return res.status(400).json({ error: "Missing required fields: name and email are required."});
+}
+
+res.send(`Hello, ${name}`);
+});
+
+app.get( '/user/:id', (req, res) => {
+  const userId =req.params.id;
+  res.send( `User ${userId} profile`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
